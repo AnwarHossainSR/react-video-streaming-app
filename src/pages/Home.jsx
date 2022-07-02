af4858d4d16f6638d137dbb604b82b7389150530
AnwarHossainSR <mahedianwar@gmail.com>
2022-07-02 11:26:30 +0600

    design video card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,38 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import Card from '../components/Card';
+import React from 'react';
+import styled from 'styled-components';
+import Card from '../components/Card';
 
-// const Container = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   flex-wrap: wrap;
-// `;
-
-// const Home = () => {
-//   return (
-//     <Container>
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//       <Card />
-//     </Container>
-//   );
-// };
-
-// export default Home;
-import React from 'react'
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
