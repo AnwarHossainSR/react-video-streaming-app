2076454e087fd02c2dc6078998ea2f3b36d30487
AnwarHossainSR <mahedianwar@gmail.com>
2022-07-02 10:36:47 +0600

    design navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
-const App = () => {
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <Container>
+      <BrowserRouter>
+        <Main>
+          <Navbar />
+          <Wrapper>
+            <Routes>
+              <Route path='/'>
+                <Route index element={<Home />} />
+              </Route>
+            </Routes>
+          </Wrapper>
+        </Main>
+      </BrowserRouter>
+    </Container>
+  );
 }
 
-export default App
+export default App;
