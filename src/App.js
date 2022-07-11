fdf033897a67ea605d6383aab60aa8c09475b0e7
Restyled.io <commits@restyled.io>
2022-07-11 15:56:17 +0000

    Restyled by clang-format

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import {useState} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import styled, {ThemeProvider} from 'styled-components';
+
 import Menu from './components/Menu';
 import Navbar from './components/Navbar';
-import { darkTheme, lightTheme } from './utils/Theme';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Video from './pages/Video';
 import SignIn from './pages/SignIn';
+import Video from './pages/Video';
+import {darkTheme, lightTheme} from './utils/Theme';
 
 const Container = styled.div`
   display: flex;
@@ -14,7 +15,7 @@ const Container = styled.div`
 
 const Main = styled.div`
   flex: 7;
-  background-color: ${({ theme }) => theme.bg};
+  background-color: ${({theme}) => theme.bg};
 `;
 const Wrapper = styled.div`
   padding: 22px 96px;
@@ -27,24 +28,28 @@ function App() {
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Container>
         <BrowserRouter>
-          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Menu darkMode={darkMode} setDarkMode={
+    setDarkMode} />
           <Main>
             <Navbar />
             <Wrapper>
               <Routes>
                 <Route path='/'>
-                  <Route index element={<Home />} />
-                  <Route path='signin' element={<SignIn />} />
+                  <Route index element={
+<Home />} />
+                  <Route path='signin' element={<SignIn />
+}
+/>
                   <Route path='video'>
-                    <Route path=':id' element={<Video />} />
-                  </Route>
-                </Route>
+                    <Route path=':id' element={<Video / >
+}
+/>
+                  </Route > </Route>
               </Routes>
-            </Wrapper>
-          </Main>
-        </BrowserRouter>
-      </Container>
-    </ThemeProvider>
+    </Wrapper>
+          </Main></BrowserRouter>
+      </Container><
+    /ThemeProvider>
   );
 }
 
